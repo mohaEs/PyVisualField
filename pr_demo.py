6ba13755c81931ee99715f7f71e4c119bb7a5ff7
mohaEs <moha.academic@gmail.com>
2021-10-18 10:35:59 -0400

    demo updated

--- pr_demo.py
+++ pr_demo.py
@@ -75,6 +75,49 @@ df_pdp = RvisualFields.getpdp(df_pd) # get PD probability values
 df_gip = RvisualFields.getglp(df_gi) # get global indices probability values
 
 
+
+
+
+# another example to show required fields
+df_VFs_py = Rvfprogression.data_vfseries()
+columns2preserve = ['eyeid','righteye','age', 'duration', 
+                     's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8',
+                      's9', 's10', 's11', 's12', 's13', 's14', 's15', 's16',
+                       's17', 's18', 's19', 's20', 's21', 's22', 's23', 's24',
+                        's25', 's26', 's27', 's28', 's29', 's30', 's31', 's32',
+                         's33', 's34', 's35', 's36', 's37', 's38', 's39', 's40',
+                         's41',  's42', 's43', 's44', 's45', 's46', 's47', 's48',
+                          's49', 's50', 's51', 's52', 's53', 's54']
+df_VFs_py = df_VFs_py[columns2preserve]
+df_VFs_py.rename(columns={'s1':'l1', 's2':'l2', 's3':'l3', 
+                                      's4':'l4', 's5':'l5', 's6':'l6', 's7':'l7',
+                                      's8':'l8', 's9':'l9', 's10':'l10',
+                                      's11':'l11', 's12':'l12', 's13':'l13',
+                                      's14':'l14', 's15':'l15','s16':'l16',
+                                      's17':'l17', 's18':'l18', 's19':'l19',
+                      's20':'l20', 's21':'l21', 's22':'l22',
+                      's23':'l23', 's24':'l24','s25':'l25', 's26':'l26', 
+                      's27':'l27', 's28':'l28', 's29':'l29', 's30':'l30', 
+                      's31':'l31', 's32':'l32','s33':'l33', 's34':'l34',
+                      's35':'l35', 's36':'l36', 's37':'l37', 's38':'l38', 
+                      's39':'l39', 's40':'l40', 's41':'l41', 
+                         's42':'l42', 's43':'l43', 's44':'l44', 's45':'l45', 
+                         's46':'l46', 's47':'l47', 's48':'l48',
+                          's49':'l49', 's50':'l50', 's51':'l51', 's52':'l52',
+                          's53':'l53', 's54':'l54'}, inplace=True)
+df_VFs_py.rename(columns={"eyeid": "id"}, inplace=True)
+df_VFs_py = df_VFs_py.replace({'righteye':{0:'OS', 1:'OD'}})
+df_VFs_py.rename(columns={"righteye": "eye"}, inplace=True)
+df_VFs_py.insert(1, "time", '00:00:00')
+df_VFs_py.insert(1, 'date', 1000)
+df_VFs_py['date'] = pandas.to_datetime(df_VFs_py['date'], errors='coerce')
+df_VFs_py.insert(1, 'type', 'SIT')
+df_VFs_py.insert(1, 'fpr', 0)
+df_VFs_py.insert(1, 'fnr', 0)
+df_VFs_py.insert(1, 'fl', 0)
+df_td, df_tdp, df_gi, df_gip, df_pd, df_pdp, gh = RvisualFields.getallvalues(df_VFs_py)
+
+
 ######
 ###### Get sample dataset as R dataframe 
 df_VFs_r= robjects.r['vfctrSunyiu24d2']
@@ -95,9 +138,6 @@ df_VFs_r= robjects.r['vfctrSunyiu24d2']
 td = np.random.randint(low=-35, high=5, size=(54,))
 Rvfprogression.plotValues(td, title= 'Total Deviation',
                                  save=True, filename='td', fmt='png')
-tdp = np.float16(np.random.rand(54)/10)
-Rvfprogression.plotProbabilities(tdp, title= 'Total Deviation Probablity',
-                                 save=True, filename='tdp', fmt='png')
 
 # more realistic example:
 ind_td_start=df_VFs_py.columns.get_loc("l1")
@@ -109,10 +149,10 @@ df_tdp = df_tdp.fillna(0)
 td = df_td.iloc[0,ind_td_start:ind_td_end+1].to_numpy().astype(np.int8())
 print(td.shape)
 print(type(td.shape))
-tdp = df_tdp.iloc[0,ind_td_start:ind_td_end+1].to_numpy().astype(np.float16())
+tdp = df_tdp.iloc[0, ind_td_start:ind_td_end+1].to_numpy().astype(np.float16())
 Rvfprogression.plotProbabilities(tdp, title= 'Total Deviation Probablity',
                                  save=True, filename='tdp', fmt='png')
-
+# make sure to use plt.close('all'), if you are using it in a loop
 
 #################
 
