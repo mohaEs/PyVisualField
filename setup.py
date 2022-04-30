fe015b35949ee770ecc090945709ca14499ae733
mohaEs <moha.academic@gmail.com>
2022-04-29 23:33:03 -0400

    youtube video

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="PyVisualFields",
-    version="1.0.1",
+    version="1.0.2",
     author="Mohammad Eslami",
     author_email="Mohammad_eslami@meei.harvard.edu",
     description="A python toolkit for visual field analysis",
